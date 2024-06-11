12ab716b768f070f16d2ccb4133b3fd2e065af24
Aviral Singh <avsingh@zoox.com>
2024-06-11 11:59:41 -0700

    typos and spacing

--- train_gpt2.py
+++ train_gpt2.py
@@ -15,58 +15,60 @@ class GPTConfig:
     n_embd: int = 768 # embedding dimension
 
 class CausalSelfAttention(nn.Module):
-    def __init__(self,config):
+
+    def __init__(self, config):
         super().__init__()
-        assert config.n_embd % config.n_head==0
+        assert config.n_embd % config.n_head == 0
         # key, query, value projections for all heads, but in a batch
-        self.c_attn = nn.Linear(config.n_embd, 3*config.n_embd)
+        self.c_attn = nn.Linear(config.n_embd, 3 * config.n_embd)
         # output projection
         self.c_proj = nn.Linear(config.n_embd, config.n_embd)
+        self.c_proj.NANOGPT_SCALE_INIT = 1
         # regularization
         self.n_head = config.n_head
-        self.n_embd = config.n_head
-        # not really a 'bias' , more of a mask but following the OpenAI/HF naming though
+        self.n_embd = config.n_embd
+        # not really a 'bias', more of a mask, but following the OpenAI/HF naming though
         self.register_buffer("bias", torch.tril(torch.ones(config.block_size, config.block_size))
-                            .view(1,1,config.block_size, config.block_size))
+                                     .view(1, 1, config.block_size, config.block_size))
 
-    def forward(self,x):
-        B, T, C= x.size() # batch size, sequence length, embedding dimensionality (n_embd)
-        # calculate query, key, values for all heads in batch and move head forward to be the batch
-        # nh is the "number of heads", hs is "head size", and C (number of channels) = nh * hs
-        # e.g. in GPT2 (124M), n+head=12, hs=64, so nh*hs=C=768 channels in the Transformer
-
-        qkv= self.c_attn(x)
-        q,k,v= qkv.split(self.n_embd, dim=2)
-        q= q.view(B,T,self.n_head,C//self.n_head).transport(1,2) # (B,nh,T,hs)
-        k= k.view(B,T,self.n_head,C//self.n_head).transport(1,2) # (B,nh,T,hs)
-        v= v.view(B,T,self.n_head,C//self.n_head).transport(1,2) # (B,nh,T,hs)
-        # attention (materializes the large (T,T) matrix for all the queries and keys)
-        att= (q @ k.transpose(-2,-1)) * (1.0/math.sqrt(k.size[-1]))
-        att = att.masked_fill(self.bias[:,:,:T,:T]==0, float('-inf'))
-        att= F.softmax(att,dim=-1)
-        y = att@ v # (B, nh, T, T) x (B, nh, T, hs) -> (B, nh, T, hs)
-        y = y.transpose(1,2).contiguous().view(B, T, C)
+    def forward(self, x):
+        B, T, C = x.size() # batch size, sequence length, embedding dimensionality (n_embd)
+        # calculate query, key, values for all heads in batch and move head forward to be the batch dim
+        # nh is "number of heads", hs is "head size", and C (number of channels) = nh * hs
+        # e.g. in GPT-2 (124M), n_head=12, hs=64, so nh*hs=C=768 channels in the Transformer
+        qkv = self.c_attn(x)
+        q, k, v = qkv.split(self.n_embd, dim=2)
+        k = k.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
+        q = q.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
+        v = v.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
+        y = F.scaled_dot_product_attention(q, k, v, is_causal=True) # flash attention
+        y = y.transpose(1, 2).contiguous().view(B, T, C) # re-assemble all head outputs side by side
+        # output projection
         y = self.c_proj(y)
         return y
-class MLP (nn.Module):
-    def __init__(self,config):
-         super().__init__()
-         self.c_fc= nn.Linear(config.n_embd, 4* config.n_embd)
-         self.gelu = nn.GELU(approximate='tanh')
-         self.c_proj = nn.Linear(4*config.n_embd, config.n_embd)
 
-    def forward(self,x):
-        x= self.c_fc(x)
-        x= self.gelu(x)
-        x= self.c_proj(x)
+class MLP(nn.Module):
+
+    def __init__(self, config):
+        super().__init__()
+        self.c_fc    = nn.Linear(config.n_embd, 4 * config.n_embd)
+        self.gelu    = nn.GELU(approximate='tanh')
+        self.c_proj  = nn.Linear(4 * config.n_embd, config.n_embd)
+        self.c_proj.NANOGPT_SCALE_INIT = 1
+
+    def forward(self, x):
+        x = self.c_fc(x)
+        x = self.gelu(x)
+        x = self.c_proj(x)
         return x
+
 class Block(nn.Module):
     def __init__(self,config):
         super().__init__()
         self.ln_1 = nn.LayerNorm(config.n_embd)
         self.attn = CausalSelfAttention(config)
         self.ln_2 =nn.LayerNorm(config.n_embd)
-        self.nlp= MLP(config)
+        self.mlp= MLP(config)
 
     # Mote We want the residual path to be clean from supervision to input
     # which means the residual path should not be add and norm
@@ -127,7 +129,7 @@ class GPT(nn.Module):
         #       | Output Embedding |
         #       --------------------
     @classmethod
-    def from_pretrain(cls,model_type):
+    def from_pretrained(cls,model_type):
         """Loads pretrained GPT-2 model weights from huggingface
 
         Args:
@@ -169,6 +171,11 @@ class GPT(nn.Module):
         for k in sd_keys_hf:
             if any(k.endswith(w) for w in transposed):
                 # special treatment for the Conv1D weights we need to transpose
+                sd_hf_shape=sd_hf[k].shape[::-1];
+                sd_shape=sd[k].shape
+                print(f'{k}')
+                print(f'sd_hf[k].shape[::-1] ==>{sd_hf_shape}')
+                print(f'sd_shape ==>{sd_shape}')
                 assert sd_hf[k].shape[::-1] == sd[k].shape
                 with torch.no_grad():
                     sd[k].copy_(sd_hf[k].t())
@@ -179,3 +186,9 @@ class GPT(nn.Module):
                     sd[k].copy_(sd_hf[k])
 
         return model
+
+#---------------------------------------------------------------------------#
+
+if __name__=="__main__":
+    model= GPT.from_pretrained('gpt2')
+    print("Loaded the Model")
